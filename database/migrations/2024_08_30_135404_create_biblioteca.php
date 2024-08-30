33137e6166fe7908407137b867bd9dd115b82357
vitor <vitorhugkrodrigues@gmail.com>
2024-08-30 11:26:21 -0300

    create/update tables

---
+++ database/migrations/2024_08_30_135404_create_biblioteca.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('biblioteca', function (Blueprint $table) {
+            $table->id();
+            $table-> string('NOME');
+            $table-> string('IMAGEM');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('biblioteca');
+    }
+};
