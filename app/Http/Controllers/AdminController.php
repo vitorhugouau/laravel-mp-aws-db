4696e99266d50be3cd699daaa5131273a5cb5e28
vitor <vitorhugkrodrigues@gmail.com>
2024-09-16 15:40:26 -0300

    fail

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,27 +15,23 @@ class AdminController extends Controller
 
     // Lida com a submissão do formulário de login
     public function login(Request $request)
-    {
-        // Validação dos dados de email e senha
-        $credentials = $request->validate([
-            'email' => 'required|string',
-            'password' => 'required|string',
-        ]);
+{
+    // Validação dos dados de email e senha
+    $credentials = $request->validate([
+        'email' => 'required|string',
+        'password' => 'required|string',
+    ]);
 
-        // Dados de autenticação
-        // $credentials = $request->only('email', 'password');
-
-        // Tenta fazer o login
-        echo "<pre>";
-        var_dump($credentials);
-        echo "</pre>";
-        if (Auth::attempt(credentials: $credentials)) {
-            // Se o login for bem-sucedido, redireciona o usuário
-            // return redirect()->intended('control');
-            var_dump('asdfiwf');
-            return redirect()->view('/control');
-        }
-
-        // Se o login falhar, retorna de volta com uma mensagem de erro
+    // Tenta fazer o login
+    if (Auth::attempt($credentials)) {
+        // Se o login for bem-sucedido, redireciona o usuário
+        return redirect()->intended('control');
     }
+
+    // Se o login falhar, retorna de volta com uma mensagem de erro
+    return back()->withErrors([
+        'email' => 'As credenciais não correspondem aos nossos registros.',
+    ]);
+}
+
 }
