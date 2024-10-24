53481c1c08fa0f549c805dfc1b9ed913449c094d
vitor <vitorhugkrodrigues@gmail.com>
2024-10-24 11:27:03 -0300

    tela de pagamento

--- resources/views/pagamento/teste.blade.php
+++ resources/views/pagamento/teste.blade.php
@@ -90,57 +90,6 @@
             display: none;
         }
     </style>
-    <style>
-        *{
-            user-select: none;
-        }
-        .compra{
-            left: 20px;
-            align-items: center;
-            justify-content: center;
-            display: flex;
-            /* gap: 15px; */
-            padding: 10px;
-        }
-        #boton{
-            background-color: rgb(243, 0, 0);
-            color: rgb(255, 255, 255);
-            border: 5px;
-            border-radius: 5px;
-            cursor: pointer;
-            transition: all 0.3s ease;
-            white-space: nowrap;
-            font-size: 14px;
-            width: 150px;
-            height: 20px;
-            left: 20px;
-            align-items: center;
-            justify-content: center;
-            display: flex;
-    
-        }
-        #boton:hover{
-            transform: scale(1.3);
-        }
-        .main-container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 20px;
-        }
-
-        .container-image {
-            flex-basis: 100%; 
-            display: block;
-        }
-        .payment-container{
-            align-items: center;
-            justify-content: center;
-            margin-right: 10%;
-            flex-basis: 50%;
-        }
-
-    </style>
 </head>
 <body background="../../assets/img/rio.jpg">
     <nav>
@@ -170,24 +119,7 @@
         </ul>
     </nav>
     <br><br><br>
-<div class="main-container">
-    <div class="container">
-        <div class="container-image" >
-            <div class="image-container">
-                <div style="background-image: url('data:image/jpeg;base64,{{ $imagem->imagem }}'); background-size: cover; background-position: center; width: auto; height: 20rem; display: flex; align-items: center; justify-content: flex-end; flex-direction: column; user-select: none !important" class="image" data-title="{{ $imagem->nome }}">
-                    <!-- Exibe a imagem armazenada no banco de dados em Base64 -->
-                    <img id="{{ $imagem->id }}" class="imagemaaa" src="{{ asset("assets/image.png") }}" style="height: 250px !important; width: 50% !important; opacity: 0.6 !important; user-select: none">
-                    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; background: white; width: 40rem; height: 40px; " >
-                        <h3>{{ $imagem->nome }}</h3>
-                        <div class="compra">
-                            <!-- <button id="boton" onclick="window.location.href='{{ route('teste') }}'">COMPRAR IMAGEM</button> -->
-                            {{-- <button id="boton" onclick="buyImage({{ $imagem->id }})">COMPRAR IMAGEM</button> --}}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
     <div class="payment-container">
         <div class="payment-header">Selecione o método de pagamento</div>
 
@@ -239,7 +171,7 @@
                     </div>
                 </div>
 
-                <button type="submit" class="submit-button" id="boton" onclick="buyImage({{ $imagem->id }})" >Efetuar pagamento</button>
+                <button type="submit" class="submit-button">Efetuar pagamento</button>
             </form>
         </div>
 
@@ -253,50 +185,16 @@
                     <input type="number" id="pix-cpf" name="pix-cpf" placeholder="Informe seu CPF/CPNJ" oninput="if(this.value.length > 14) this.value = this.value.slice(0, 14);" required>
                 </div>
 
-                <button type="submit" class="submit-button" id="boton" onclick="buyImage({{ $imagem->id }})" >Efetuar pagamento </button>
+                <button type="submit" class="submit-button">Efetuar pagamento com Pix</button>
             </form>
         </div>
     </div>
-</div>
+
     <script>
         function togglePaymentForm(method) {
             document.getElementById('credit-card-form').style.display = (method === 'credit') ? 'block' : 'none';
             document.getElementById('pix-form').style.display = (method === 'pix') ? 'block' : 'none';
         }
-        
-        const buyImage = (id) => {
-        const imagemComprada = document.getElementById(id);
-        imagemComprada.style.display = 'none';
-    };
-    </script>
-
-    
-
-    </script>
-    <script>
-        document.addEventListener('keydown',
-            function(e){
-                if (e.key === "F12" || (e.ctrlKey && 
-            e.shiftKey && e.key === 'I' )) {
-                e.preventDefault();
-            }
-            }
-        );
-
-        document.addEventListener('contextmenu',
-            function (e){
-                e.preventDefault();
-            }
-        );
-    </script>
-  
-  <script>
-        document.getElementById('sair').addEventListener('click', function() {
-                document.getElementById('logout-form').submit();
-            });
-            function bloquearBotaoDireito(event){
-                event.preventDefault();
-            }
     </script>
     
 </body>
