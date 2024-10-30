3c5d997c24dcdf13712f39881fb636ca19829367
vitor <vitorhugkrodrigues@gmail.com>
2024-10-30 13:51:37 -0300

    feat: control add icons

--- resources/views/adm/control.blade.php
+++ resources/views/adm/control.blade.php
@@ -8,7 +8,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="/css/adm/panelcontrol.css">
 </head>
-
+<style>
+    .icon {
+        filter: invert(1); 
+    }
+</style>
 <body>
 
     <div class="container-geral">
@@ -18,33 +22,12 @@
                     <h1>PAINEL DE CONTROLE</h1>
                 </div>
                 <div class="panel-content">
-                    <ul style="display: flex; align-items: center; gap: 20px; list-style-type: none; padding: 0; margin: 0;"></ul>>
-                    <div style="display: flex; align-items: center; gap:13px;">
-                        <img src="{{ asset("assets/carrinho-de-compras.png") }}" alt="Ícone" style="width: 16px; height: 16px; margin-left: 5px; vertical-align: middle; filter: invert(1);"> 
-                        <li style="display: flex; align-items: center; gap: 20px; list-style-type: none; padding: 0; margin: 0;">><a href="{{route('usuarios.index')}}">Gerenciar Usuários</a></li>
-                    </div>    
-                        <!-- <li><a href="upload.php">Adicionar Imagem ao Banco</a></li> -->
-                        {{-- <li><a href="consult_img/consult_control.php">Consultar Imagem</a></li> --}}
-                    <div style="display: flex; align-items: center; gap:13px;">
-                        <img src="{{ asset("assets/carrinho-de-compras.png") }}" alt="Ícone" style="width: 16px; height: 16px; margin-left: 5px; vertical-align: middle; filter: invert(1);"> 
-                        <li><a href="{{route('uploads')}}">Adicionar Imagens na Biblioteca</a></li>
-                    </div>
-                    <div style="display: flex; align-items: center; gap:13px;">
-                        <img src="{{ asset("assets/carrinho-de-compras.png") }}" alt="Ícone" style="width: 16px; height: 16px; margin-left: 5px; vertical-align: middle; filter: invert(1);"> 
-                        <li><a href="{{route('imagens.table')}}">Consultar Imagens</a></li>
-                    </div>
-                    <div style="display: flex; align-items: center; gap:13px;">
-                        <img src="{{ asset("assets/carrinho-de-compras.png") }}" alt="Ícone" style="width: 16px; height: 16px; margin-left: 5px; vertical-align: middle; filter: invert(1);"> 
-                        <li><a href="{{route('clientes.index2')}}">Gerenciar Clientes</a></li>
-                    </div>
-                    <div style="display: flex; align-items: center; gap:13px;">
-                        <img src="{{ asset("assets/carrinho-de-compras.png") }}" alt="Ícone" style="width: 16px; height: 16px; margin-left: 5px; vertical-align: middle; filter: invert(1);"> 
-                        <li><a href="{{route('sales.index')}}">Consultar Vendas</a></li>
-                    </div>
-                        {{-- <li><a href="add_img/panel_control.php">Adicionando Imagens nos Albuns</a></li> --}}
-                        <!-- <li><a href="cliente/crud_serviço.php">Gerenciador de clientes</a></li> -->
-                        <!-- <li><a href="artista/crud_artista.php">Artistas</a></li> -->
-                        <!-- <li><a href="equipamento/crud_equipamento.php">Equipamentos</a></li> -->
+                    <ul>
+                        <li><img src="{{ asset('assets/portrait.png') }}" alt="Ícone" class="icon"><a href="{{ route('usuarios.index') }}">Gerenciar Usuários</a></li>
+                        <li><img src="{{ asset('assets/picture.png') }}" alt="Ícone" class="icon"><a href="{{ route('uploads') }}">Adicionar Imagens na Biblioteca</a></li>
+                        <li><img src="{{ asset('assets/eye.png') }}" alt="Ícone" class="icon"><a href="{{ route('imagens.table') }}">Consultar Imagens</a></li>
+                        <li><img src="{{ asset('assets/users-alt.png') }}" alt="Ícone" class="icon"><a href="{{ route('clientes.index2') }}">Gerenciar Clientes</a></li>
+                        <li><img src="{{ asset('assets/carrinho-de-compras.png') }}" alt="Ícone" class="icon"><a href="{{ route('sales.index') }}">Consultar Vendas</a></li>
                     </ul>
                 </div>
             </div>
@@ -57,14 +40,10 @@
                 </form>
                 <p>
                     <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        <i class="bi bi-box-arrow-right"></i> Sair
-                    </a>
+                        <i class="bi bi-box-arrow-right"></i> Sair </a>
                 </p>
-
-
-
             </div>
-        </div>
+        </div>      
         <div class="main-content">
             <img style="height: 115vh; width: 100vw;" src="../../assets/img/ti2.jpg" alt="">
         </div>
