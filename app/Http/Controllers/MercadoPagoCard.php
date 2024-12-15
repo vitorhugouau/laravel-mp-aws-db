80f6f0280b9e4ee21e5e725b7c0dc2a85b09e2f2
vitorhugouau <vitorhugkrodrigues@gmail.com>
2024-12-14 23:34:21 -0300

    error: view pix

--- app/Http/Controllers/MercadoPagoCard.php
+++ app/Http/Controllers/MercadoPagoCard.php
@@ -26,99 +26,77 @@ class MercadoPagoCard extends Controller
     }
 
     public function createPaymentPreference(Request $request)
-{
-    Log::info('Iniciando criação da preferência de pagamento.');
-
-    $validated = $request->validate([
-        'imagem_id' => 'required',
-        'valor' => 'required',
-    ]);
-
-    Log::info('Dados validados com sucesso.', ['validated_data' => $validated]);
-
-    $imagemId = $validated['imagem_id'];
-    $valor = (float) $validated['valor'];
-
-    Log::info('Valores extraídos.', ['imagem_id' => $imagemId, 'valor' => $valor]);
-
-    $this->authenticate();
-    Log::info('Autenticação realizada com sucesso.');
-
-    $user = Auth::user();
-    Log::info('Usuário autenticado.', ['user_id' => $user->id, 'user_email' => $user->email]);
-
-    $product1 = [
-        "id" => (string) $request->imagem_id,
-        "title" => "Produto " . $request->imagem_id,
-        "description" => "Descrição do Produto " . $request->imagem_id,
-        "currency_id" => "BRL",
-        "quantity" => 1,
-        "unit_price" => (float) $request->valor
-    ];
-
-    Log::info('Produto criado.', ['product' => $product1]);
-
-    $items = [$product1];
-
-    $payer = [
-        "name" => $request->user()->name,
-        "email" => $request->user()->email
-    ];
-
-    Log::info('Informações do comprador criadas.', ['payer' => $payer]);
-
-    $requestData = $this->createPreferenceRequest($items, $payer);
-    $requestData['external_reference'] = $request->imagem_id;
-
-    Log::info('Dados para preferência criados.', ['request_data' => $requestData]);
-
-    $client = new PreferenceClient();
-
-    try {
-        Log::info('Tentando criar a preferência de pagamento.');
-        $preference = $client->create($requestData);
-        Log::info('Preferência de pagamento criada com sucesso.', ['init_point' => $preference->init_point]);
-        return redirect($preference->init_point);
-
-    } catch (MPApiException $error) {
-        Log::error('Erro ao criar preferência de pagamento.', [
-            'message' => $error->getMessage(),
-            'code' => $error->getCode(),
+    {
+      
+        $request->validate([
+            'imagem_id' => 'required|integer',
+            'valor' => 'required|numeric|min:0',
         ]);
-        return response()->json(['error' => 'Erro ao criar a preferência de pagamento.'], 500);
+
+        $this->authenticate();
+
+      
+        $product1 = [
+            "id" => (string) $request->imagem_id,
+            "title" => "Produto " . $request->imagem_id,
+            "description" => "Descrição do Produto " . $request->imagem_id,
+            "currency_id" => "BRL",
+            "quantity" => 1,
+            "unit_price" => (float) $request->valor
+        ];
+
+        $items = [$product1];
+
+        $payer = [
+            "name" => "Vitor",
+            "surname" => "Hugo",
+            "email" => "nicolas@gmail.com"
+        ];
+
+       
+        $requestData = $this->createPreferenceRequest($items, $payer);
+        $requestData['external_reference'] = $request->imagem_id;
+
+        $client = new PreferenceClient();
+
+        try {
+            $preference = $client->create($requestData);
+            return redirect($preference->init_point);
+
+        } catch (MPApiException $error) {
+            Log::error('Erro ao criar preferência de pagamento: ', [
+                'message' => $error->getMessage(),
+                'code' => $error->getCode(),
+                
+            ]);
+            return response()->json(['error' => 'Erro ao criar a preferência de pagamento.'], 500);
+        }
     }
-}
 
-private function createPreferenceRequest($items, $payer): array
-{
-    Log::info('Criando dados da preferência para o Mercado Pago.', ['items' => $items, 'payer' => $payer]);
+    private function createPreferenceRequest($items, $payer): array
+    {
+        $paymentMethods = [
+            "excluded_payment_methods" => [],
+            "installments" => 12,
+            "default_installments" => 1
+        ];
 
-    $paymentMethods = [
-        "excluded_payment_methods" => [],
-        "installments" => 12,
-        "default_installments" => 1
-    ];
+        $backUrls = [
+            'success' => route('mercadopago.success'),
+            'failure' => route('biblioteca'),
+        ];
 
-    $backUrls = [
-        'success' => route('mercadopago.success'),
-        'failure' => route('biblioteca'),
-    ];
-
-    $requestData = [
-        "items" => $items,
-        "payer" => $payer,
-        "payment_methods" => $paymentMethods,
-        "back_urls" => $backUrls,
-        "statement_descriptor" => "MINHA LOJA",
-        "expires" => false,
-        "auto_return" => 'approved',
-    ];
-
-    Log::info('Dados da preferência criados com sucesso.', ['request_data' => $requestData]);
-
-    return $requestData;
-}
-
+        return [
+            "items" => $items,
+            "payer" => $payer,
+            "payment_methods" => $paymentMethods,
+            "back_urls" => $backUrls,
+            "statement_descriptor" => "MINHA LOJA",
+            "external_reference" => "1234567890",
+            "expires" => false,
+            "auto_return" => 'approved',
+        ];
+    }
 
     public function getPreferenceById($preferenceId)
     {
