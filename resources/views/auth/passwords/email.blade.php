c97e7551dd2c7dd3f957bff01c26f10f70fe0697
vitor <vitorhugkrodrigues@gmail.com>
2024-12-16 08:26:01 -0300

    feat: add password reset flow via email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resetar Senha</title>
-    <link rel="stylesheet" href="/css/login/parte3.css">
+    <link rel="stylesheet" href="/css/login/parte3-email.css">
 </head>
 
 {{-- <body>
