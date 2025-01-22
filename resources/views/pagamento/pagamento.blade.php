549f344e3ff7730ab142c4917125f8f09ef16165
vitor <vitorhugkrodrigues@gmail.com>
2025-01-22 16:40:51 -0300

    update Ui desing sucess-payment-page

--- resources/views/pagamento/pagamento.blade.php
+++ resources/views/pagamento/pagamento.blade.php
@@ -15,63 +15,7 @@
     @include('partials.nav')
 
     <br><br><br>
-    {{-- <div class="main-container">
-        <div class="container">
-            <div class="container-image">
-                <div class="image-container">
-                    <div
-                        style="background-image: url('{{ $imagem->url_marca_dagua }}'); background-size: cover; background-position: center; width: auto; height: 300px; display: flex; align-items: center; justify-content: flex-end; flex-direction: column; user-select: none !important">
-                        <div
-                            style="display: flex; flex-direction: column; align-items: center; justify-content: center; width: 40rem; height: 40px;">
-                            <div class="compra">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
-
-    {{-- <div id="paymentModal" style="display: none;">
-            <div class="modal-content">
-                <span id="closeModal" style="cursor: pointer;">&times;</span>
-                <h2>Pagamento via Pix</h2>
-                <div id="qrCodeContainer"></div>
-                <button id="cancelPayment">Cancelar</button>
-                <button id="confirmPayment">Confirmar Pagamento</button>
-            </div>
-        </div> --}}
-
-    {{-- <div class="pay">
-            <form action="{{ route('mercadopago.create') }}" method="POST" id="paymentForm">
-                @csrf
-                <input type="hidden" name="imagem_id" value="{{ $imagem->id }}">
-                <div class="container1">
-                    <button class="button" onclick="checkSelection(event)">
-                        Pagamento via Pix
-                    </button>
-                </div>
-            </form>
-
-            <form action="{{ route('mercadopago.createCard') }}" method="post" id="paymentForm">
-                @csrf
-                <input type="hidden" name="imagem_id" value="{{ $imagem->id }}">
-                <div class="container2">
-                    <h2>Área de Confirmação</h2>
-                    <div class="planos">
-                        <label>
-                            <input type="radio" name="valor" value="{{ $imagem->valor }}" style="margin-right: 8px;">
-                            R$ {{ number_format($imagem->valor, 2, ',', '.') }} por esta imagem<br>
-                        </label>
-                        <strong>→ Confirme Clicando no Botão Abaixo</strong>
-                    </div>
-                    <button class="button" onclick="checkSelection()">Pagamento via Cartão</button>
-
-                    <div class="info">
-                        Inclui a nossa <a href="#">licença padrão</a>.<br>
-                    </div>
-                </div>
-            </form>
-        </div> --}}
+    
 
     {{-- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ --}}
 
@@ -80,42 +24,7 @@
         <div class="payment-options">
             <h2 class="payment-title">Escolha como pagar</h2>
 
-            {{-- <div class="payment-method">
-            <input type="radio" id="combine" name="payment_method" class="payment-radio">
-            <label for="combine">
-                <div class="method-details">
-                    <span>Combinar 2 meios de pagamento</span>
-                    <span class="new-option">NOVA OPÇÃO</span>
-                </div>
-            </label>
-        </div> --}}
-
-            {{-- <div class="payment-method">
-            <input type="radio" id="saldo" name="payment_method" class="payment-radio">
-            <label for="saldo">
-                <div class="method-details">
-                    <img src="link_para_imagem_saldo" alt="Ícone Saldo" class="method-icon">
-                    <div>
-                        <span>Saldo no Mercado Pago</span>
-                        <span class="method-subtitle">Saldo: R$ 6,90</span>
-                    </div>
-                </div>
-            </label>
-        </div> --}}
-
-            {{-- <div class="payment-method">
-            <input type="radio" id="pix" name="payment_method" class="payment-radio">
-            <label for="pix">
-                <div class="method-details">
-                    <img src="{{ asset('assets/pagamento/pix.png') }}" alt="Ícone Pix" class="method-icon">
-                    <div>
-                        <span>Pix</span>
-                        <span class="method-subtitle">Aprovação imediata</span>
-                        <span class="new-option">NOVA OPÇÃO</span>
-                    </div>
-                </div>
-            </label>
-        </div> --}}
+            
             <form action="{{ route('mercadopago.create') }}" method="post" id="paymentForm">
                 @csrf
                 <input type="hidden" name="imagem_id" value="{{ $imagem->id }}">
@@ -182,18 +91,7 @@
 
 
         </div>
-        {{-- <div class="payment-method">
-        <input type="radio" id="boleto" name="payment_method" class="payment-radio">
-        <label for="boleto">
-            <div class="method-details">
-                <img src="link_para_imagem_boleto" alt="Ícone Boleto" class="method-icon">
-                <div>
-                    <span>Boleto</span>
-                    <span class="method-subtitle">Aprovação em 1 a 2 dias úteis</span>
-                </div>
-            </div>
-        </label>
-    </div> --}}
+       
 
         <!-- Resumo da compra -->
         <div class="purchase-summary">
--- resources/views/pagamento/success.blade.php
+++ resources/views/pagamento/success.blade.php
@@ -6,97 +6,103 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagamento Aprovado</title>
     <link rel="stylesheet" href="/css/biblioteca/type2.css">
+    <link rel="stylesheet" href="/css/biblioteca/type-sucess.css">
 </head>
 @include('partials.nav')
+
 <body>
-    <div class="container">
-        <h1>Pagamento Aprovado</h1>
+    <div class="payment-page">
+        <div class="payment-options">
+            <h1>Pagamento Aprovado</h1>
 
-        <p><strong>ID do Pagamento:</strong> {{ $payment_id }}</p>
-        <p><strong>Status do Pagamento:</strong> {{ $status }}</p>
-
-        @if($imagem)
-            <h3>Imagem Comprada:</h3>
-            <div class="main-container">
-                <div class="container">
-                    <div class="container-image">
-                        <div class="image-container">
-                            <div id="imagemkk"
-                                style="background-image: url('{{ $imagem->url_original }}'); background-size: cover; background-position: center; width: auto; height: 300px; display: flex; align-items: center; justify-content: flex-end; flex-direction: column; user-select: none !important">
+            <p><strong>ID do Pagamento:</strong> {{ $payment_id }}</p>
+            <p><strong>Status do Pagamento:</strong> {{ $status }}</p>
+        </div>
+        <div class="purchase-summary">
+            @if ($imagem)
+                <h3>Imagem Comprada:</h3>
+                <div class="main-container">
+                    <div class="container">
+                        <div class="container-image">
+                            <div class="image-container">
+                                <div
+                                    style="background-image: url('{{ $imagem->url_original }}'); background-size: cover; background-position: center; width: 690px; height: 387px; display: flex; align-items: center; justify-content: flex-end; flex-direction: column; user-select: none !important;border-radius:4%">
                                     <div
-                                    style="display: flex; flex-direction: column; align-items: center; justify-content: center; width: 40rem; height: 40px;">
-                                    <div class="compra">
+                                        style="display: flex; flex-direction: column; align-items: center; justify-content: center; width: 40rem; height: 40px;">
+                                        <div class="compra">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        @else
-            <p>Informações da imagem não disponíveis.</p>
-        @endif
-        <button class="btn btn-primary" onclick="downloadImage()">Baixar Imagem</button>
-        <br>
-        <a href="{{ route('biblioteca') }}" class="btn btn-primary">Voltar à Página Inicial</a>
+            @else
+                <p>Informações da imagem não disponíveis.</p>
+            @endif
 
+            <div class="button-container">
+                <button class="btn btn-primary" onclick="downloadImage()">Baixar Imagem</button>
+                <a href="{{ route('biblioteca') }}" class="btn btn-primary">Voltar à Página Inicial</a>
+            </div>
+
+
+        </div>
     </div>
     <script>
+        // if (performance.navigation.type === performance.navigation.TYPE_RELOAD) {
+        //     if (sessionStorage.getItem('formSubmitted')) {
 
-        if (performance.navigation.type === performance.navigation.TYPE_RELOAD) {
-            if (sessionStorage.getItem('formSubmitted')) {
-                
-                window.location.href = "{{route('minhas.compras') }}";
-            } else {
-                sessionStorage.setItem('formSubmitted', true);
-            }
-        }
+        //         window.location.href = "{{ route('minhas.compras') }}";
+        //     } else {
+        //         sessionStorage.setItem('formSubmitted', true);
+        //     }
+        // }
     </script>
     <script>
         const imagemElement = document.getElementById('imagemkk');
         const imagemUrl = imagemElement.style.backgroundImage.slice(5, -2);
 
         function downloadImage() {
-            const link = document.createElement('a'); 
-            link.href = imagemUrl; 
+            const link = document.createElement('a');
+            link.href = imagemUrl;
             link.download = 'imagem.jpg';
-            document.body.appendChild(link); 
-            link.click(); 
+            document.body.appendChild(link);
+            link.click();
             document.body.removeChild(link);
         }
-
-
     </script>
     <style>
-            .btn {
-                display: inline-block;
-                padding: 12px 24px;
-                font-size: 16px;
-                font-weight: bold;
-                color: #fff;
-                background-color: #3498db;
-                text-align: center;
-                border-radius: 8px;
-                text-decoration: none;
-                transition: background-color 0.3s, transform 0.3s;
-                box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-                cursor: pointer;
-                width: 250px;
-                margin: 3px;
-            }
+        .btn {
+            display: inline-block;
+            padding: 12px 24px;
+            font-size: 15px;
+            font-weight: bold;
+            color: #fff;
+            background-color: #000000;
+            text-align: center;
+            border-radius: 8px;
+            text-decoration: none;
+            transition: background-color 0.3s, transform 0.3s;
+            box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+            cursor: pointer;
+            width: 250px;
+            margin: 3px;
+        }
 
-            .btn:hover {
-                background-color: #2980b9;
-                transform: translateY(-2px);
-                box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.15);
-            }
+        .btn:hover {
+            background-color: #ffffff;
+            color: #000000;
+            transform: translateY(-2px);
+            box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.15);
+        }
 
-            .btn:active {
-                background-color: #1c5d8b;
-                transform: translateY(0px);
-                box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-            }
-        </style>
+        .btn:active {
+            background-color: #1c5d8b;
+            transform: translateY(0px);
+            box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+        }
+    </style>
 </body>
 
 </html>
